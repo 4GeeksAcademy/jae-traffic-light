2128ffd88abaddce5201a66a5f929673ce47429b
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-02-07 01:55:31 +0000

    final build

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState("red")
+	const [selectedColor, setSelectedColor] = useState("yellow");
+
 	return (
 		<div className="traffic-light">
-			<div className="light red glow"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
-			
+
+			<div 
+			onClick={() => setSelectedColor("red")}
+			className={"light red" + ((selectedColor === "red") ? " glow" : "")}></div>
+			<div 
+			onClick={() => setSelectedColor("yellow")}
+			className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}></div>
+			<div
+			onClick={() => setSelectedColor("green")}
+			className={"light green" + ((selectedColor === "green") ? " glow" : "")}></div>
+
 		</div>
 	);
-};
+}
 
 export default Home;
